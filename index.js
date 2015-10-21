eccbadd147eed27fe82203d36f8548bcb1d4660b
Tejas Deshpande <tejasd@outlook.com>
2015-10-21 19:18:55 -0400

    Mostly done, need to add database support

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 'use strict';
 var app = require('express')();
-var bodyParser = require('body-parser');
 
-// Third party middleware
-app.use(bodyParser.json());
+// Setup SMS format Regex
+var smsRegex = new RegExp(/MD [A-H]/);
+var phoneNoRegex = new RegExp(/\+1[0-9]{10}/);
 
-app.post('/smsReceived', function(req, res) {
-	console.log(req.body);
-})
+// End point for twilio to hit after receives text messages
+// from members of the audience
+app.get('/smsReceived', function(req, res) {
+
+	// Should prevent any unwanted data
+	var text = smsRegex.exec(req.query.Body);
+	text = text ? text[0] : '';
+	var sender = phoneNoRegex.exec(req.query.From);
+	sender = sender ? sender[0] : '';
+
+	// Actually testing
+	if (text.length > 0) { // If we reveived SMS in the correct format add to tallly
+		console.log(text[3]);
+		console.log('Valid Vote received');
+		// 'insert into Votes (PhoneNo, Vote) values ('+15132231672', 'B') on duplicate key update Vote = 'B';'
+	} else {
+		console.log('Invalid Vote');
+	}
+});
 
 app.set('port', (process.env.PORT || 5000));
 
