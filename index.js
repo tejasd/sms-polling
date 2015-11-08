09997a4d11502385e3612765fe297e6b6b197bab
Tejas Deshpande <tejasd@outlook.com>
2015-11-08 16:51:16 -0500

    Ready to be tested

--- index.js
+++ index.js
@@ -3,58 +3,59 @@ var app = require('express')();
 var pg = require('pg');
 var connectionString = process.env.DATABASE_URL;
 
-var keywordsDict = require('./utilities/loadWords.js').createDictionaryOfValidKeywords();
+var voteKeywords = require('./utilities/loadWords.js').voteKeywords;
+var eventKeywords = require('./utilities/loadWords.js').eventKeywords;
+var votes_table = require('./schema.js').votes;
+
+console.log(voteKeywords);
+console.log(eventKeywords);
 
 // Setup SMS format Regex
 // var smsRegex = new RegExp(/MD [A-H]/);
-var phoneNoRegex = new RegExp(/\+1[0-9]{10}/);
+var phoneNoRegex = new RegExp(/1[0-9]{10}/);
 
-// End point for twilio to hit after receives text messages
+// End point for nexmo to hit after it receives text messages
 // from members of the audience
 app.get('/smsReceived', function(req, res) {
+
+	// No point in keeping the request open since we aren't communicating with nexmo
+	res.sendStatus(200);
 
 	// Should prevent any unwanted data
 	// var text = smsRegex.exec(req.query.Body);
 	// text = text ? text[0] : '';
-	var sender = phoneNoRegex.exec(req.query.From);
+	var sender = phoneNoRegex.exec(req.query.msisdn);
 	sender = sender ? sender[0] : '';
 
-	// Parsing response
-	var choice = req.query.Body.toLowerCase();
+	// // If deciding event based on Phone number sent to
+	// var pollInstance = eventKeywords[sender];
 
-	choice = keywordsDict[choice];
-	console.log(choice);
+	// If deciding event based on prefix keyword (eg. JHALAK in 'JHALAK Qurbani')
+	var pollInstance = req.query.text ? req.query.text.toLowerCase().split(" ")[0] : '';
+	console.log(pollInstance);
+	pollInstance = eventKeywords[pollInstance];
+
+	// Parsing response
+	var choice = req.query.text ? req.query.text.toLowerCase().split(" ")[1] : '';
+	choice = voteKeywords[pollInstance] ? voteKeywords[pollInstance][choice] : '';
+	choice = choice !== undefined ? choice : '';
+
+	console.log('polling for' + pollInstance + 'with vote: ' + choice);
 
 	// Actually testing
-	if (choice) { // If we reveived SMS in the correct format add to tallly
+	if (pollInstance && (choice.length > 0)) { // If we reveived SMS in the correct format add to tallly
 		console.log(choice);
 		console.log('Valid Vote received');
-		// 'insert into Votes (PhoneNo, Vote) values ('+15132231672', 'B') on duplicate key update Vote = 'B';'
-		// insert it into the database
-		// Get a Postgres client from the connection pool
-	    // pg.connect(connectionString, function(err, client, done) {
-	    //     // Handle connection errors
-	    //     if(err) {
-	    //       done();
-	    //       console.log(err);
-	    //       res.status(500).json({ success: false, data: err});
-	    //     }
-
-	    //     // SQL Query > Insert Data
-	    //     client.query("INSERT INTO \"Votes\" (phoneno, vote) values($1, $2)", [sender, text[3]], function(err, result) {
-	    //     	if (err) {
-	    //     		console.log('error: ' + err);
-	    //     		res.status(500).json({ success: false, data: err});
-	    //     	} else {
-	    //     		console.log('success: ' + result);
-	    //     		res.status(200).json({ success: true, data: result});
-	    //     		done();
-	    //     	}
-	    //     });
-	    // });
+		
+		registerVote(pollInstance, choice);
+	    
 	} else {
 		console.log('Invalid Vote');
 	}
+});
+
+app.post('/smsReceived', function(req, res) {
+	res.sendStatus(200);
 });
 
 app.set('port', (process.env.PORT || 5000));
@@ -66,3 +67,28 @@ var server = app.listen(app.get('port'), function () {
 
   console.log('SMS audience polling is being hosted at http://%s:%s', host, port);
 });
+
+var registerVote = function(event_name, choice) {
+
+	var insertQuery = votes_table.insert(votes_table.event_name.value(event_name), votes_table.choice.value(choice)).toQuery();
+	// var updateQuery = votes_table.update()
+	console.log(insertQuery);
+	
+	pg.connect(connectionString, function(err, client, done) {
+	        // Handle connection errors
+	        if(err) {
+	          done();
+	          console.log(err);
+	        }
+
+	        // SQL Query > Insert Data
+	        client.query(insertQuery.text, insertQuery.values, function(err, result) {
+	        	if (err) {
+	        		console.log('error: ' + err);
+	        	} else {
+	        		console.log('success: ' + result);
+	        		done();
+	        	}
+	        });
+	    });
+}
--- utilities/loadWords.js
+++ utilities/loadWords.js
@@ -2,19 +2,40 @@
 // into an easily consumable 'associated array'
 
 var fs = require('fs')
-var keywordsDict = JSON.parse(fs.readFileSync('./utilities/teamNames.json', 'utf8'));
+var config = JSON.parse(fs.readFileSync('./utilities/config.json', 'utf8'));
 
-var result = {}
+var pathPrefix = './utilities/keywords/'
 
-var createDictionaryOfValidKeywords = function() {
-	for (key in keywordsDict) {
-		keyWordsList = keywordsDict[key];
+var readMultipleKeywordFiles = function() {
+	var result = {};
+
+	for (var i = 0; i < config.keywordFiles.length; ++i) {
+		var compKeywords = JSON.parse(fs.readFileSync(pathPrefix + config.keywordFiles[i], 'utf8'));
+		result[config.events[i]] = createDictionaryOfValidKeywords(compKeywords);
+	}
+
+	return result;
+}
+
+var createDictionaryOfValidKeywords = function(givenDict) {
+	var newDict = {}
+	for (key in givenDict) {
+		keyWordsList = givenDict[key];
 		for (var i = 0; i < keyWordsList.length; ++i) {
-			result[keyWordsList[i].toLowerCase()] = key.toLowerCase();
+			newDict[keyWordsList[i].toLowerCase()] = key.toLowerCase();
 		}
 	}
 
-	return result
+	return newDict;
 }
 
-module.exports.createDictionaryOfValidKeywords = createDictionaryOfValidKeywords 
+module.exports.voteKeywords = readMultipleKeywordFiles();
+module.exports.eventKeywords = config.keywordToEvent;
+
+
+
+// {
+// 	"jhalak": {jhalak's keywords dictionary},
+// 	"chaos": {chaos' keyword dictionary}
+// }
+
